dbf62c1d55632eeee7b35e5441057b424e504f3d
Justine11289 <ju891128@gmail.com>
2022-05-17 14:40:27 +0800

    class

---
+++ cs_22/string_plus.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string a = "IU!IU!";
+    string b = "Go Go Go!";
+    string c = a+b;
+    int x = 1;
+    int y = 2;
+    int z = x+y;
+    cout << c << endl;
+    cout << z << endl;
+    return 0;
+}
